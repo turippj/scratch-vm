0bb43f9eee1106accff1e0cc8d930a36a3a93f7a
MakTak <justaway22@gmail.com>
2019-06-05 01:04:53 +0900

    extensionの雛型を追加

---
+++ src/extensions/scratch3_newblock/index.js
@@ -0,0 +1,39 @@
+const ArgumentType = require('../../extension-support/argument-type');
+const BlockType = require('../../extension-support/block-type');
+const Cast = require('../../util/cast');
+const log = require('../../util/log');
+
+class Scratch3NewBlocks {
+    constructor (runtime) {
+        this.runtime = runtime;
+    }
+
+    getInfo () {
+        return {
+            id: 'newblocks',
+            name: 'New Blocks',
+            blocks: [
+                {
+                    opcode: 'writeLog',
+                    blockType: BlockType.HAT,
+                    text: 'log [TEXT]',
+                    arguments: {
+                        TEXT: {
+                            type: ArgumentType.STRING,
+                            defaultValue: "hello"
+                        }
+                    }
+                }
+            ],
+            menus: {
+            }
+        };
+    }
+
+    writeLog (args) {
+        const text = Cast.toString(args.TEXT);
+        log.log(text);
+    }
+}
+
+module.exports = Scratch3NewBlocks;
